0987eb107d7d9262669638ec19f66c72ec1e749e
Van Charles TRAN <vctran@vctran1.(none)>
2013-09-17 15:09:22 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nwol::Application.config.secret_token = 'd028c2369741a40720ee02bd23fe4bc287a51c6fddde575ec050fa22d14c8bc1ea763f18a26594a1854a9fb584e5abc8a0f28ea1236b99f2f70e9df260666a28'
